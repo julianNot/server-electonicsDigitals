1d14728fd2669ad2c6f621185a6a983e7c0db834
Julian Ardila <julian.ardila@uptc.edu.co>
2022-03-23 22:21:53 -0500

    rutas para clientes, facturas por id y listas

--- app.js
+++ app.js
@@ -3,9 +3,77 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hola otra vez')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/clients', (req, res) => {
+    res.json([
+        {
+            id : 1,
+            name : 'Harry',
+            document : '102920'
+
+        },
+        {
+            id : 2,
+            name : 'Mafe',
+            document : '102920'
+
+        },
+        {
+            id : 3,
+            name : 'Julian',
+            document : '102920'
+
+        },
+    ])
+})
+
+app.get('/clients/:id', (req, res) => {
+    const {id} = req.params
+    res.json({
+        id,
+        name : 'lista clients uwu'
+    })
+})
+
+app.get('/voices', (req, res) => {
+    res.json([
+        {
+            id : 1,
+            name : 'bulls S.A.S'
+        },
+        {
+            id : 2,
+            name : 'Copias S.A'
+        },
+        {
+            id : 3,
+            name : 'Dell inc'
+        },
+        {
+            id : 4,
+            name : 'Uptc'
+        },
+    ])
+})
+
+app.get('/voices/:id',(req, res) => {
+    const { id } = req.params
+    res.json({
+        id : 4,
+        name : 'Uptc'
+    })
+})
+
+app.get('/clients/:clientId/voices/:voiceId', (req, res) => {
+    const {clientId, voiceId} = req.params
+    res.json({
+        clientId,
+        voiceId
+    })
+})
+
+app.listen(port, () =>{
+    console.log('En ejecucion puerto : ' + port)
 })
