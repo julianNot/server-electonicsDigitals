4aaf2dd2e0bac04a56464e06936cfdeb964f1563
Julian Ardila <julian.ardila@uptc.edu.co>
2022-03-24 12:36:30 -0500

    se añadio Cors para solucionar problema de seguridad

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const routerApi = require('./routers')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 const port = 3000
 
 app.use(express.json())
